4ef6c40a0596dbe80e907b18c668fd2c2eba5991
D Paras Kumar <paraskumar475@gmail.com>
2026-02-28 15:56:48 +0530

    update

---
+++ 6 Fuction and Recursion/50.c
@@ -0,0 +1,13 @@
+// Write a function named greet that prints "Hello, World!" when called.
+#include<stdio.h>
+void Hello();
+
+void Hello(){
+    printf("Hello, World !");
+}
+int main(int argc, char const *argv[])
+{
+    printf("This is First function here \n");
+    Hello();
+    return 0;
+}
