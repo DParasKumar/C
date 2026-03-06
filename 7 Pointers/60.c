78ef600506018192670b16331a06b83176248a54
D Paras Kumar <paraskumar475@gmail.com>
2026-03-06 16:47:44 +0530

    update

---
+++ 7 Pointers/60.c
@@ -0,0 +1,12 @@
+// Write a program to change the value of an integer variable using a pointer and the * operator.
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num = 5;
+    int *ptr = &num;
+
+    *ptr = 87;
+    printf("The value of num is %d", num);
+    /* code */
+    return 0;
+}
