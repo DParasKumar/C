58dcd70b77bcf85d74ec38510db4abfd0976970d
D Paras Kumar <paraskumar475@gmail.com>
2026-03-08 13:03:23 +0530

    d

---
+++ 9 Arrays/array_traversal.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int arr[] = { 4,6,4,3,3};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nthe marks of student are: %d", arr[i]);
+    }
+    
+    return 0;
+}
