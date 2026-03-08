ce3b6c62a49ffdef3e7ee6c74391f78651f38527
D Paras Kumar <paraskumar475@gmail.com>
2026-03-08 13:07:50 +0530

    update

--- 9 Arrays/array_traversal.c
+++ 9 Arrays/array_traversal.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    int arr[] = { 4,6,4,3,3};
+    int marks[5];
+
     for (int i = 0; i < 5; i++)
     {
-        printf("\nthe marks of student are: %d", arr[i]);
+        printf("Please enter marks for student %d:\n", i + 1);
+        scanf("%d", &marks[i]);
+    }
+    
+
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nthe marks of student %d are: %d",i+1, marks[i]);
     }
     
     return 0;
