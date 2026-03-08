e14b878dde04b93721cc86524dc0c1c0824ea0d5
D Paras Kumar <paraskumar475@gmail.com>
2026-03-08 12:45:23 +0530

    s

--- 9 Arrays/array_declaration.c
+++ 9 Arrays/array_declaration.c
@@ -2,7 +2,21 @@
 int main(int argc, char const *argv[])
 {   int marks[5]; // array declaration
     
-
+    printf("Please enter marks for student 1:\n");
+    scanf("%d", &marks[0]);
+    printf("Please enter marks for student 2:\n");
+    scanf("%d", &marks[1]);
+    printf("Please enter marks for student 3:\n");
+    scanf("%d", &marks[2]);
+    printf("Please enter marks for student 4:\n");
+    scanf("%d", &marks[3]);
+    printf("Please enter marks for student 5:\n");
+    scanf("%d", &marks[4]);
     
+    printf("\nMarks of student 1 are : %d", marks[0]);
+    printf("\nMarks of student 2 are : %d", marks[1]);
+    printf("\nMarks of student 3 are : %d", marks[2]);
+    printf("\nMarks of student 4 are : %d", marks[3]);
+    printf("\nMarks of student 5 are : %d", marks[4]);
     return 0;
 }
