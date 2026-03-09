d0a9f59aeb4661007cd9b27908a2977a892f0f1c
D Paras Kumar <paraskumar475@gmail.com>
2026-03-09 16:34:05 +0530

    3d dimensional array

---
+++ 9 Arrays/three_dimensional_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int marks[2][3][4];
+
+    for (int row = 0; row < 2; row++)
+    {
+        for (int col = 0; col < 3; col++)
+        {
+            for (int t = 0; t < 4; t++)
+            {
+            printf("Enter the marks for student: %d for subject: %d \n", row+1,col+1);
+            scanf("%d", &marks[row][col]);
+            }
+            
+            
+        }
+        
+    }
+    
+
+
+    return 0;
+}
