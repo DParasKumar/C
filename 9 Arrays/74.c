604f3cbd2643b3f486763dab5464e696eaa954b1
D Paras Kumar <paraskumar475@gmail.com>
2026-03-23 23:46:52 +0530

    create a program to merge two sorted arrays.

--- 9 Arrays/74.c
+++ 9 Arrays/74.c
@@ -1,38 +1,63 @@
 // create a program to merge two sorted arrays.
-#include<stdio.h>
-void merge_sorted_array(int arr1[], int size1, int arr2[], int size2, int arr3, int size3);
-void merge_sorted_array(int arr1[], int size1, int arr2[], int size2, int arr3, int size3){
+#include <stdio.h>
+
+void merge_sorted_arrays(int arr1[], int size1, int arr2[], int size2, int arr3[], int size3)
+{
     int i = 0, j = 0, k = 0;
     while (k < size3)
     {
-        if j == size 2 || (arr1[i]< arr2[j])
+
+        if (j == size2 || i < size1 && arr1[i] < arr2[j])
         {
             arr3[k] = arr1[i];
             i++;
-        }else{
+        }
+        else
+        {
             arr3[k] = arr2[j++];
         }
-        
+
+        // if (j == size2)
+        // {
+        //     arr3[k] = arr1[i];
+        //     i++;
+        // }
+        // else if (i == size1)
+        // {
+        //     arr3[k] = arr2[j++];
+        // }
+
+        // else if (arr1[i] < arr2[j])
+        // {
+        //     arr3[k] = arr1[i];
+        //     i++;
+        // }
+        // else
+        // {
+        //     arr3[k] = arr2[j++];
+        // }
+
+        k++;
     }
-    
 }
 
 int main(int argc, char const *argv[])
 {
-    
-    int arr1[] = {1,3,5,7,9};
-    int arr2[] = {2,4,6,8,10,12,14};
-    int arr3[12]; 
-    printf("\nWelcome to merging sorted arrays:");
-    printf("\nHere is the merged array:");
-    printt_arr(arr3,12);
+    int arr1[5] = {1, 3, 5, 7, 9};
+    int arr2[7] = {2, 4, 6, 8, 10, 12, 14};
+    int arr3[12];
+
+    printf("welcome to merging sorted arrays");
+    printf("\n\n Herre is the merged array:");
+    merge_sorted_arrays(arr1, 5, arr2, 7, arr3, 12);
+    print_arr(arr3,12);
     return 0;
 }
 
-void printt_arr(int arr[], int size){
+void print_arr(int arr[], int size)
+{
     for (int i = 0; i < size; i++)
     {
-        printf("%d", arr[i]);
+        printf(" %d", arr[i]);
     }
-    
 }
