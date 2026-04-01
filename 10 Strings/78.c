66b109a161afc5b7be59c8974496398f352709fb
D Paras Kumar <paraskumar475@gmail.com>
2026-04-01 08:31:00 +0530

    Read a line of text from the user using fgets and then print it using puts.

---
+++ 10 Strings/78.c
@@ -0,0 +1,13 @@
+// Read a line of text from the user using fgets and then print it using puts.
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    char name[240] ;
+    printf("Welcome to showing use of fgets and puts\n");
+    printf("Please, Enter your full name:");
+    fgets(name, sizeof(name), stdin);
+
+    printf("The Name you entered is :");
+    puts(name);
+    return 0;
+}
