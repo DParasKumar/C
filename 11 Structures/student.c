c4944c0fe3ae594efa9a13f9be665469c5d9685f
D Paras Kumar <paraskumar475@gmail.com>
2026-04-23 15:34:53 +0530

    struct

--- 11 Structures/student.c
+++ 11 Structures/student.c
@@ -8,6 +8,16 @@ struct student
 };
 int main(int argc, char const *argv[])
 {
-    struct student student;
+    struct student student1;
+    printf("Please enter student details");
+    printf("\nEnter the roll no:");
+    scanf("%d", &student1.rollno);
+    printf("Enter the name: ");
+    scanf(" %9s", student1.name);
+    printf("Now Enter the marks:");
+    scanf("%f", &student1.marks);
+
+    printf("Here are the student details\n");
+    printf("RollNo.: %d, Name: %s, Marks: %f", student1.rollno, student1.name, student1.marks);
     return 0;
 }
