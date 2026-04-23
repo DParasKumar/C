b9d3f9b7bc59cb6b6e9251f8ccb21a7df4b6e663
D Paras Kumar <paraskumar475@gmail.com>
2026-04-23 15:22:21 +0530

    ff

---
+++ 11 Structures/student.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+struct student
+{
+    int rollno;
+    char name[10];
+    float marks;
+};
+int main(int argc, char const *argv[])
+{
+    struct student student;
+    return 0;
+}
