813408bacd5dd15ab10eddebe8933d103a00aa9e
D Paras Kumar <paraskumar475@gmail.com>
2026-04-24 15:15:12 +0530

    structure pointer

---
+++ 11 Structures/pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+struct student
+{
+    int rollno;
+    char name[10];
+    float marks;
+};
+
+int main(int argc, char const *argv[])
+{
+
+    int a =5;
+    int *ptr = &a;
+
+    struct student student = {1, "Ram", 45.5};
+    struct student *stu_ptr = &student;
+    
+    return 0;
+}
