ca91b62a1e9e4cc6d393cb703e1b39947c2f5012
D Paras Kumar <paraskumar475@gmail.com>
2026-04-24 17:08:06 +0530

    f

--- 11 Structures/pointer.c
+++ 11 Structures/pointer.c
@@ -15,5 +15,6 @@ int main(int argc, char const *argv[])
     struct student student = {1, "Ram", 45.5};
     struct student *stu_ptr = &student;
     
+    printf("RollNo: %d, Name:%s, Marks: %.2f", stu_ptr->rollno, stu_ptr->name, stu_ptr->marks );
     return 0;
 }
