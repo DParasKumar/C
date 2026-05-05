b1f3a45d9bd5f0aeb62ac3df540799c931867b0a
D Paras Kumar <paraskumar475@gmail.com>
2026-05-05 16:48:15 +0530

    Define a car structure with fields for make, model, year and color.

---
+++ 11 Structures/90.c
@@ -0,0 +1,21 @@
+// Define a car structure with fields for make, model, year and color.
+
+#include<stdio.h>
+#include<string.h>
+
+struct Car
+{
+    char make[25];
+    char model[25];
+    int year[4];
+    char color[15];
+};
+
+typedef struct Car Car;
+
+int main(int argc, char const *argv[])
+{
+     
+    return 0;
+}
+
