2786a3cd067ab3168744f0d8e00addb498778c5b
D Paras Kumar <paraskumar475@gmail.com>
2026-06-15 17:25:04 +0530

    create a program that reads integers from a file and calculates their sum.

--- 13 file input and output/100.c
+++ 13 file input and output/100.c
@@ -1,5 +1,6 @@
 // create a program that reads integers from a file and calculates their sum.
 #include <stdio.h>
+
 int main(int argc, char const *argv[])
 {
     const char filename[] = "numbers.txt";
@@ -10,17 +11,18 @@ int main(int argc, char const *argv[])
         printf("File could not be opened\n");
         return 1;
     }
+
     int temp;
     int sum = 0;
-    int read_status;
-    do
-    {
-        read_status = fscanf( file,"%d", &temp);
-        sum += temp;
-        /* code */
-    } while (read_status == 1);
 
-    printf("\n the sum of all numbers is %d");
+    // Reads until fscanf fails to match an integer (EOF or bad input)
+    while (fscanf(file, "%d", &temp) == 1)
+    {
+        sum += temp;
+    }
+
+    printf("\nThe sum of all numbers is %d\n", sum); // Bug 2 fix: pass `sum`
+
     fclose(file);
     file = NULL;
 
