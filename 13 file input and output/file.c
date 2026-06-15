5b6a7739d69a92c01f0a1e9df8e47701a11ae418
D Paras Kumar <paraskumar475@gmail.com>
2026-06-15 16:22:19 +0530

    files

--- 13 file input and output/file.c
+++ 13 file input and output/file.c
@@ -1,28 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
     FILE *file_pointer;
     char text[100];
 
-    file_pointer = fopen("coding.txt","r");
+    file_pointer = fopen("coding.txt", "r");
     if (file_pointer == NULL)
     {
         printf("invalid file");
         return 1;
     }
 
-    fgets(text, 100, file_pointer);
-    printf("\n%s", text);
-    
+    // fgets(text, 100, file_pointer);
+    // printf("\n%s", text);
+
+    int c;
+    do
+    {
+        c = fgetc(file_pointer);
+        printf("%c", c);
+    } while (c != EOF);
+
     int result = fclose(file_pointer);
-    if (result ==0)
+    if (result == 0)
     {
         file_pointer = NULL;
         /* code */
-    } else{
+    }
+    else
+    {
         printf("fiel was not closed properly");
     }
-    
+
     return 0;
 }
