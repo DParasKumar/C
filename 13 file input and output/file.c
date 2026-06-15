1c895649d761322414ee353b1943981c3d868fad
D Paras Kumar <paraskumar475@gmail.com>
2026-06-15 16:08:17 +0530

    file handleing

---
+++ 13 file input and output/file.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    FILE *file_pointer;
+    char text[100];
+
+    file_pointer = fopen("coding.txt","r");
+    if (file_pointer == NULL)
+    {
+        printf("invalid file");
+        return 1;
+    }
+
+    fgets(text, 100, file_pointer);
+    printf("\n%s", text);
+    
+    int result = fclose(file_pointer);
+    if (result ==0)
+    {
+        file_pointer = NULL;
+        /* code */
+    } else{
+        printf("fiel was not closed properly");
+    }
+    
+    return 0;
+}
