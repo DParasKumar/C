236b5caadf08cdf4af1ef753ddfa3030bd13087f
D Paras Kumar <paraskumar475@gmail.com>
2026-06-16 00:46:47 +0530

    write a program that takes uers input and writes it to a file, ensuring each entry is on a new line.

--- 13 file input and output/102.c
+++ 13 file input and output/102.c
@@ -1,1 +1,30 @@
 // write a program that takes uers input and writes it to a file, ensuring each entry is on a new line.
+
+#include<stdio.h>
+#include<string.h>
+int main(int argc, char const *argv[])
+{
+    printf("Welcome to our writing user input to file\n");
+    FILE *file = fopen("user-input.txt", "w");
+    if (!file)
+    {
+        printf("Could not open file.");
+        return 1;
+    }
+
+    char input[100];
+    while(1)
+    {
+        /* code */
+        fgets(input, 100, stdin);
+        if (strcmp(input, "exit\n") == 0) break;
+        
+        fputs(input, file);
+
+    } 
+    fclose(file);
+    file = NULL;
+    
+    
+    return 0;
+}
