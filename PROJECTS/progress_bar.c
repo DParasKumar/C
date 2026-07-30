cfd2c78b16e4d7fb65c28722c5c3af77280294c3
D Paras Kumar <paraskumar475@gmail.com>
2026-07-30 15:54:43 +0530

    multiple tasks

--- PROJECTS/progress_bar.c
+++ PROJECTS/progress_bar.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+void print_bar(int id, int progress);
+const int BAR_LENGTH = 100;
 int main(int argc, char const *argv[])
 {
-    const int BAR_LENGTH = 100;
+    print_bar(1,25);
+    print_bar(9,100);
+    print_bar(4,90);
+    print_bar(6,0);
 
+    return 0;
+}
+
+void print_bar(int id, int progress)
+{
     /* code */
-    int id = 3, progress = 40;
-    int bars_to_show = progress * BAR_LENGTH / 100;
+    // int id = 3, progress = 40;
+    int bars_to_show = (progress * BAR_LENGTH) / 100;
     printf("\nTask %d: [", id);
     for (int i = 0; i < BAR_LENGTH; i++)
     {
-        if (i <= bars_to_show)
+        if (i < bars_to_show)
         {
             printf("=");
         }
@@ -20,5 +30,4 @@ int main(int argc, char const *argv[])
     }
 
     printf("] %d%%", progress);
-    return 0;
 }
