bcad3de4cae1feb2117edf255c359e8542a0fafd
D Paras Kumar <paraskumar475@gmail.com>
2026-07-30 15:51:00 +0530

    first run

--- PROJECTS/progress_bar.c
+++ PROJECTS/progress_bar.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    const int BAR_LENGTH = 50;
+    const int BAR_LENGTH = 100;
 
     /* code */
     int id = 3, progress = 40;
-    int bars_to_show = progress / 100 * BAR_LENGTH;
+    int bars_to_show = progress * BAR_LENGTH / 100;
     printf("\nTask %d: [", id);
     for (int i = 0; i < BAR_LENGTH; i++)
     {
-        if (i < bars_to_show)
+        if (i <= bars_to_show)
         {
             printf("=");
         }
