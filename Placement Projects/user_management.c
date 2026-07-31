015388c4130119204e190601af94034471079e8e
D Paras Kumar <paraskumar475@gmail.com>
2026-07-31 22:50:48 +0530

    update

--- Placement Projects/user_management.c
+++ Placement Projects/user_management.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_USERS 10
 
@@ -12,52 +13,86 @@ User users[MAX_USERS];
 int user_count = 0;
 
 void register_user();
-void login_user();  //returns the user index
+int login_user(); // returns the user index, or -1 if login fails
+
 int main(int argc, char const *argv[])
 {
     int option;
+
     while (1)
     {
-
         printf("\n Welcome to user management");
-        printf("\n 1.Register");
-        printf("\n 1.Login");
-        printf("\n 1.Exit");
-        printf("\n Select an option");
+        printf("\n 1. Register");
+        printf("\n 2. Login");
+        printf("\n 3. Exit");
+        printf("\n Select an option: ");
         scanf("%d", &option);
-        /* code */
+
+        switch (option)
+        {
+        case 1:
+            register_user();
+            break;
+        case 2:
+        {
+            int user_index = login_user();
+            if (user_index >= 0)
+            {
+                printf("\n Login successful! Welcome, %s", users[user_index].username);
+            }
+            else
+            {
+                printf("\n Login failed! Incorrect username or password.");
+            }
+            break;
+        }
+        case 3:
+            printf("\nExiting program.");
+            return 0;
+        default:
+            printf("\nInvalid option. Please try again.");
+            break;
+        }
     }
-    switch (option)
-    {
-    case 1:
-    register_user();
-        break;
-    case 2:
-    int user_index = login_user();
-    if (user_index >= 0)
-    {
-        printf("")  ;          
-    }else{
-        printf("\n Login failed ! Incorrect username or password.");
-    }
-    
-        break;
-    case 3:
-        printf("\nExiting program.");
-        return 0;
-        break;
-    default:
-        printf("\nInvalid option. Please try again.");
-        break;
-    }
+
     return 0;
 }
 
-void register_user(){
-    printf("Dummmy");
+void register_user()
+{
+    if (user_count >= MAX_USERS)
+    {
+        printf("\n User limit reached. Cannot register more users.");
+        return;
+    }
+
+    printf("\n Enter username: ");
+    scanf("%29s", users[user_count].username);
+    printf("\n Enter password: ");
+    scanf("%29s", users[user_count].password);
+
+    user_count++;
+    printf("\n Registration successful!");
 }
 
+int login_user()
+{
+    char username[30];
+    char password[30];
 
-void login_user(){
+    printf("\n Enter username: ");
+    scanf("%29s", username);
+    printf("\n Enter password: ");
+    scanf("%29s", password);
+
+    for (int i = 0; i < user_count; i++)
+    {
+        if (strcmp(users[i].username, username) == 0 &&
+            strcmp(users[i].password, password) == 0)
+        {
+            return i;
+        }
+    }
+
     return -1;
 }
