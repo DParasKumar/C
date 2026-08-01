59333574babd6ac762725ae54b1deef7246b828b
D Paras Kumar <paraskumar475@gmail.com>
2026-08-01 15:10:09 +0530

    function building

--- Placement Projects/Bank_Management_System.c
+++ Placement Projects/Bank_Management_System.c
@@ -1,14 +1,73 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void create_account();
+void deposit_money();
+void withdraw_money();
+void check_balance();
+
 int main(int argc, char const *argv[])
 {
-    int choice;
-    printf("***Bank Management System***");
-    printf("\n 1.Create Account");
-    printf("\n 2.Deposit Money");
-    printf("\n 3.Withdraw Money");
-    printf("\n 4.Check Balance");
-    printf("\n 5.Exit");
-    printf("\n Enter your choice:");
-    scanf("%d",&choice);
-    return 0;
+    while (1)
+    {
+
+        int choice;
+
+        printf("\n\n***Bank Management System***");
+        printf("\n 1.Create Account");
+        printf("\n 2.Deposit Money");
+        printf("\n 3.Withdraw Money");
+        printf("\n 4.Check Balance");
+        printf("\n 5.Exit");
+        printf("\n Enter your choice:");
+        scanf("%d", &choice);
+        /* code */
+
+        switch (choice)
+        {
+        case 1:
+            create_account();
+            /* code */
+            break;
+
+        case 2:
+            deposit_money();
+            /* code */
+            break;
+
+        case 3:
+            withdraw_money();
+            /* code */
+            break;
+
+        case 4:
+            check_balance();
+            /* code */
+            break;
+
+        case 5:
+            printf("\n Closing the bank, Thanks for your visit");
+            return 0;
+            break;
+
+        default:
+            printf("Invalid Choice");
+            break;
+        }
+    }
+}
+void create_account()
+{
+    printf("\nCreating Account");
+}
+void deposit_money()
+{
+    printf("\nDepositing Money");
+}
+void withdraw_money()
+{
+    printf("\nWithdrawing Money");
+}
+void check_balance()
+{
+    printf("\nCecking Balance");
 }
