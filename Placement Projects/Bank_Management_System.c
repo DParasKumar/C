e78ead56f691be8c291d4103c58768b4d1d76283
D Paras Kumar <paraskumar475@gmail.com>
2026-08-01 14:57:15 +0530

    initial point

---
+++ Placement Projects/Bank_Management_System.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int choice;
+    printf("***Bank Management System***");
+    printf("\n 1.Create Account");
+    printf("\n 2.Deposit Money");
+    printf("\n 3.Withdraw Money");
+    printf("\n 4.Check Balance");
+    printf("\n 5.Exit");
+    printf("\n Enter your choice:");
+    scanf("%d",&choice);
+    return 0;
+}
