44a6334dc75db69b44c70bc0f5f92ff3efe286d5
D Paras Kumar <paraskumar475@gmail.com>
2026-08-07 16:20:02 +0530

    complete

--- Placement Projects/sudoko_puzzel.c
+++ Placement Projects/sudoko_puzzel.c
@@ -13,17 +13,32 @@ int puzzle[9][9] = {
 };
 
 void print_puzzle(int puzzle[9][9]);
+int valid_move(int puzzle[9][9], int row, int col, int val);
+int solve_puzzle(int puzzle[9][9], int row, int col);
+
 int main(int argc, char const *argv[])
 {
+    printf("welcome to sudoko Solver !");
+    printf("\nOriginal Puzzle:");
     print_puzzle(puzzle);
+    if (solve_puzzle(puzzle, 0, 0))
+    {
+        printf("\n The puzzle is solved: ");
+        print_puzzle(puzzle);
+    }
+    else
+    {
+        printf("\n This puzzle is not solvable\n");
+    }
+
     return 0;
 }
+
 void print_puzzle(int puzzle[9][9])
 {
     printf("\n\n +--------+--------+--------+");
     for (int row = 0; row < 9; row++)
     {
-
         if (row % 3 == 0 && row != 0)
         {
             printf("\n |+-------+--------+-------+|");
@@ -42,7 +57,6 @@ void print_puzzle(int puzzle[9][9])
             }
             else
             {
-
                 printf("  ");
             }
         }
@@ -51,4 +65,73 @@ void print_puzzle(int puzzle[9][9])
     }
 
     printf("\n +--------+--------+--------+");
+}
+
+int valid_move(int puzzle[9][9], int row, int col, int val)
+{
+    // valid row
+    for (int i = 0; i < 9; i++)
+    {
+        if (puzzle[row][i] == val)   // FIX: was "=" (assignment), now "=="
+        {
+            return 0;
+        }
+    }
+
+    // valid column
+    for (int i = 0; i < 9; i++)
+    {
+        if (puzzle[i][col] == val)
+        {
+            return 0;
+        }
+    }
+
+    // valid 3x3 box
+    int r = row - row % 3;
+    int c = col - col % 3;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (puzzle[r + i][c + j] == val)
+            {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+int solve_puzzle(int puzzle[9][9], int row, int col)
+{
+    if (col == 9)
+    {
+        row++;
+        col = 0;
+        if (row == 9)
+        {
+            return 1; // Puzzle solved
+        }
+    }
+
+    if (puzzle[row][col] > 0)
+    {
+        return solve_puzzle(puzzle, row, col + 1);
+    }
+
+    for (int i = 1; i <= 9; i++)   
+    {
+        if (valid_move(puzzle, row, col, i))
+        {
+            puzzle[row][col] = i;
+            if (solve_puzzle(puzzle, row, col + 1))
+            {
+                return 1;
+            }
+            puzzle[row][col] = 0;
+        }
+    }
+    return 0;
 }
