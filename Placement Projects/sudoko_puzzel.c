76ca8bc64e17b291028c50496e29bf9377413bba
D Paras Kumar <paraskumar475@gmail.com>
2026-08-07 14:00:36 +0530

    1st phase

---
+++ Placement Projects/sudoko_puzzel.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int puzzle[9][9] = {
+    {3, 0, 0, 0, 2, 0, 0, 7, 0},
+    {9, 0, 0, 5, 0, 0, 0, 1, 4},
+    {0, 1, 6, 3, 7, 0, 0, 0, 8},
+    {2, 0, 0, 8, 0, 0, 0, 0, 1},
+    {5, 0, 0, 0, 4, 1, 8, 0, 0},
+    {0, 8, 9, 0, 0, 0, 0, 5, 0},
+    {0, 0, 5, 0, 1, 0, 2, 8, 0},
+    {0, 4, 0, 0, 0, 6, 0, 9, 3},
+    {7, 3, 1, 0, 8, 2, 0, 0, 0},
+};
+
+void print_puzzle(int puzzle[9][9]);
+int main(int argc, char const *argv[])
+{
+    print_puzzle(puzzle);
+    return 0;
+}
+void print_puzzle(int puzzle[9][9])
+{
+    printf("\n\n +--------+--------+");
+    for (int row = 0; row < 9; row++)
+    {
+        printf("\n");
+        for (int col = 0; col < 9; col++)
+        {
+            if (col % 3 == 0)
+            {
+                printf(" | ");
+            }
+            
+            printf("%d", puzzle[row][col]);
+
+        }
+        
+        printf(" |");
+    }
+    
+    printf("\n +--------+--------+");
+}
