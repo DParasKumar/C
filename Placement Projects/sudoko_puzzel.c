e04396f94aac24b277dbc825977ea01285346139
D Paras Kumar <paraskumar475@gmail.com>
2026-08-07 14:08:26 +0530

    2nd phase

--- Placement Projects/sudoko_puzzel.c
+++ Placement Projects/sudoko_puzzel.c
@@ -20,9 +20,15 @@ int main(int argc, char const *argv[])
 }
 void print_puzzle(int puzzle[9][9])
 {
-    printf("\n\n +--------+--------+");
+    printf("\n\n +--------+--------+--------+");
     for (int row = 0; row < 9; row++)
     {
+
+        if (row % 3 == 0 && row != 0)
+        {
+            printf("\n |+-------+--------+-------+|");
+        }
+
         printf("\n");
         for (int col = 0; col < 9; col++)
         {
@@ -30,13 +36,19 @@ void print_puzzle(int puzzle[9][9])
             {
                 printf(" | ");
             }
-            
-            printf("%d", puzzle[row][col]);
+            if (puzzle[row][col] != 0)
+            {
+                printf("%d ", puzzle[row][col]);
+            }
+            else
+            {
 
+                printf("  ");
+            }
         }
-        
+
         printf(" |");
     }
-    
-    printf("\n +--------+--------+");
+
+    printf("\n +--------+--------+--------+");
 }
